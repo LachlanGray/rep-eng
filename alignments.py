f15fb0173b44c17198f0fd8320635a9912156902
Lachlan <lachlan.gray2000@gmail.com>
2024-10-27 15:44:56 -0700

    cross-alignments and notebook

--- alignments.py
+++ alignments.py
@@ -84,6 +84,32 @@ class TextAlignment:
         return alignments + alignments.T
 
 
+    def cross_alignment_matrix(self, other, kernel_fn="cosine", topk=10):
+        if self.sims == {}:
+            self.compute_similarities(kernel_fn, topk)
+        if other.sims == {}:
+            other.compute_similarities(kernel_fn, topk)
+
+
+        n_self_models = len(self.models)
+        n_other_models = len(self.models)
+        alignments = torch.zeros([n_self_models, n_other_models])
+
+        for i in range(n_self_models):
+            model_i = self.models[i]
+            for j in range(n_other_models):
+                model_j = other.models[j]
+
+                sims_i = self.sims[model_i]
+                sims_j = other.sims[model_j]
+
+                overlaps = [len(set(x.tolist()) & set(y.tolist())) for x, y in zip(sims_i, sims_j)]
+
+                alignments[i,j] = sum(overlaps) / len(overlaps) / topk
+
+        return alignments
+
+
 if __name__ == "__main__":
     models = [
         "meta-llama/Llama-3.2-1B-Instruct",
